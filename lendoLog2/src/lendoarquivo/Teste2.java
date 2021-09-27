a9f46708c4b511ac14908405fff4a3bc8bf52bbf
Breno <89496407+NeverNobre@users.noreply.github.com>
2021-09-27 16:36:03 -0300

    Add files via upload

---
+++ lendoLog2/src/lendoarquivo/Teste2.java
@@ -0,0 +1,16 @@
+package lendoarquivo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Teste2 {
+
+	public static void main(String[] args) throws Exception {
+		long startTime= System.nanoTime();
+		
+		String result[];
+		BufferedReader reader= new BufferedReader(new FileReader("src/lendoarquivo/access.log"));
+		
+		Map<String, Integer> frequency= new HashMap
+	}
+}
