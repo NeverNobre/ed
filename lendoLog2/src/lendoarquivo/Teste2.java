888dd7187220de45dc0534c4f1a154cee05fb3aa
Breno <89496407+NeverNobre@users.noreply.github.com>
2021-09-27 18:01:30 -0300

    atualizado

--- lendoLog2/src/lendoarquivo/Teste2.java
+++ lendoLog2/src/lendoarquivo/Teste2.java
@@ -2,6 +2,8 @@ package lendoarquivo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Teste2 {
 
@@ -11,6 +13,40 @@ public class Teste2 {
 		String result[];
 		BufferedReader reader= new BufferedReader(new FileReader("src/lendoarquivo/access.log"));
 		
-		Map<String, Integer> frequency= new HashMap
+		HashMap<String, Integer> frequency= new HashMap<>();
+		String line= reader.readLine();
+		while (line!= null) {
+			result= line.split(" - - ");
+			
+			if (frequency.containsKey(result[0])) 
+				frequency.put(result[0], frequency.get(result[0])+1);
+			else
+				frequency.put(result[0], 1);
+			
+			line= reader.readLine();
+		}
+		
+		long finishTime= System.nanoTime()-startTime;
+		System.out.println("Tempo da montagem de dados: "+finishTime+" nanosegundos");
+		
+		reader= new BufferedReader(new FileReader("src/lendoarquivo/Lista100enderecos.txt"));
+		line=reader.readLine();
+		
+		while(line!=null) {
+			long consultaMapaInicio=System.nanoTime();
+			
+			int vezes;
+			if (frequency.containsKey(line))
+			vezes= frequency.get(line);
+			else
+				vezes=0;
+			
+			long consultaMapaDuracao=System.nanoTime()- consultaMapaInicio;
+			
+			System.out.print("IP "+line+" apareceu "+vezes+" vezes. ");
+			System.out.println("Tempo de consulta: "+consultaMapaDuracao+" nanosegundos\n");
+			
+			line= reader.readLine();
+		}
 	}
 }
